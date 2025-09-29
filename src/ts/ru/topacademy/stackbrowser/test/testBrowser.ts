df53a34e7760aeaaacba848f697a68d6570646d5
Qwexaro <Sergikstar25.gold@mail.ru>
2025-09-30 00:13:03 +0300

    add first task

---
+++ src/ts/ru/topacademy/stackbrowser/test/testBrowser.ts
@@ -0,0 +1,31 @@
+import { StackBrowser } from "../stack-browser.ts";
+
+const stackBrowser = new StackBrowser();
+
+stackBrowser.openUrl("http");
+stackBrowser.openUrl("Udp");
+stackBrowser.openUrl("tcp");
+
+console.log(stackBrowser.getCurrent());
+
+stackBrowser.back();
+
+console.log(stackBrowser.getCurrent());
+
+stackBrowser.back();
+
+console.log(stackBrowser.getCurrent());
+
+stackBrowser.back();
+
+stackBrowser.forward();
+
+console.log(stackBrowser.getCurrent());
+            
+stackBrowser.forward();
+
+console.log(stackBrowser.getCurrent());
+
+stackBrowser.forward();
+
+console.log(stackBrowser.getCurrent());
